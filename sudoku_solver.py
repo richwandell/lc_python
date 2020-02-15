714d46e1f978ad4a5daf070435b523bb15973b53
Rich Wandell <richwandell@gmail.com>
2020-02-15 10:48:30 -0500

    sudoku

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,4 +1,5 @@
 from typing import List
+import pprint
 
 
 class Solution:
@@ -6,7 +7,69 @@ class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
+        our choice
+        our constraints
+        our goal
         """
+
+        nums = list([str(n) for n in range(1, 10)])
+
+        def can_place(r, c, n):
+            can_place = False
+            # check row
+            if n not in board[r]:
+                can_place = True
+                # check col
+                for i in range(len(board)):
+                    if board[i][c] == n:
+                        can_place = False
+                        break
+            if can_place:
+                # validate sub grid
+                if c < 3:
+                    grid_x = 0
+                elif c < 6:
+                    grid_x = 1
+                else:
+                    grid_x = 2
+                if r < 3:
+                    grid_y = 0
+                elif r < 6:
+                    grid_y = 1
+                else:
+                    grid_y = 2
+                for y in range(grid_y * 3, grid_y * 3 + 3):
+                    for x in range(grid_x * 3, grid_x * 3 + 3):
+                        if board[y][x] == n:
+                            can_place = False
+                            break
+                    if not can_place:
+                        break
+
+                if can_place:
+                    return True
+            return False
+
+        def solve(r, c):
+            if c == len(board[0]):
+                c = 0
+                r += 1
+                if r == len(board):
+                    return True
+
+            if board[r][c] != ".":
+                return solve(r, c+1)
+
+            for n in nums:
+                if can_place(r, c, n):
+                    board[r][c] = n
+                    if solve(r, c+1):
+                        return True
+                    board[r][c] = "."
+            return False
+        solve(0, 0)
+
+
 
 
 
@@ -23,3 +86,7 @@ board = [
     [".", ".", ".",   "4", "1", "9",   ".", ".", "5"],
     [".", ".", ".",   ".", "8", ".",   ".", "7", "9"]
 ]
+
+s = Solution()
+s.solveSudoku(board)
+pprint.pprint(board)
