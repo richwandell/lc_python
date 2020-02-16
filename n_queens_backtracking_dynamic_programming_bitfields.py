fa8ab46030b8442c65b73d3f907210eedcb433da
Rich Wandell <richwandell@gmail.com>
2020-02-16 16:43:29 -0500

    n-queens working on bits

--- n_queens_backtracking_dynamic_programming_bitfields.py
+++ n_queens_backtracking_dynamic_programming_bitfields.py
@@ -3,70 +3,52 @@ import pprint
 
 
 class Solution:
+
     def solveNQueens(self, n: int) -> List[List[str]]:
-        used, boards, board, current = {}, [], [], []
-        test_cache = {}
-        bcols, bldiag, brdiag = 0, 0, 0
-
-        def can_place_queen(r, c):
-            cc = bcols & (1 << c)
-            ldc = bldiag & (1 << (r-c + n))
-            rdc = brdiag & (1 << (r+c))
-
-            check = cc | ldc | rdc
-            if check > 0:
-                return False
-
-            return True
 
         def solve(r):
-            nonlocal bcols, bldiag, brdiag
-            if r == n:
-                key = tuple(current)
-                if key in used:
-                    return False
-                bcols, bldiag, brdiag = 0, 0, 0
-                used[key] = True
-                return True
-
-            cache_key = tuple(current)
-            if cache_key in test_cache:
-                if not test_cache[cache_key]:
-                    return False
+            nonlocal bcols, bldiag, brdiag, current
 
             for c in range(n):
-                if can_place_queen(r, c):
-                    current.append((r, c))
-                    board[r][c] = "Q"
+                if bcols & (1 << c) | bldiag & (1 << (r - c + n)) | brdiag & (1 << (r + c)) < 1:
+                    last_path = current
 
-                    lldiag = r-c + n
-                    lrdiag = r+c
+                    if current == 0:
+                        current = 1
 
-                    bcols = bcols | (1 << c)
-                    bldiag = bldiag | (1 << lldiag)
-                    brdiag = brdiag | (1 << lrdiag)
+                    current = (current << 4) | c
 
-                    if solve(r+1):
+                    if current in exhausted_paths:
+                        current = last_path
+                        continue
+
+                    board[r * n + c] = True
+
+                    lldiag, lrdiag = r-c + n, r+c
+                    bcols, bldiag, brdiag = bcols | (1 << c), bldiag | (1 << lldiag), brdiag | (1 << lrdiag)
+
+                    if r+1 == n or solve(r+1):
+                        exhausted_paths[current] = False
                         return True
-                    board[r][c] = "."
-                    current.pop()
 
-                    bcols = bcols & ~(1 << c)
-                    bldiag = bldiag & ~(1 << lldiag)
-                    brdiag = brdiag & ~(1 << lrdiag)
+                    board[r * n + c] = False
+                    current = last_path
 
-            test_cache[cache_key] = False
+                    bcols, bldiag, brdiag = bcols & ~(1 << c), bldiag & ~(1 << lldiag), brdiag & ~(1 << lrdiag)
+
+            exhausted_paths[current] = False
             return False
 
-        row = "." * n
+        row, boards, exhausted_paths = [False] * n * n, [], {}
         while True:
-            current = []
-            board = [list(row) for _ in range(n)]
-            bcols, bldiag, brdiag = 0, 0, 0
+            board = list(row)
+            bcols, bldiag, brdiag, current = 0, 0, 0, 0
             solve(0)
-            if "Q" in board[0]:
-                board = ["".join(x) for x in board]
-                boards.append(board)
+            if True in board[:n]:
+                new_board = []
+                for i in range(0, len(board), n):
+                    new_board.append("".join(["Q" if x == True else "." for x in board[i:i+n]]))
+                boards.append(new_board)
             else:
                 break
         return boards
@@ -76,4 +58,4 @@ s = Solution()
 boards = s.solveNQueens(9)
 
 print(len(boards))
-pprint.pprint(boards, width=10)
+# pprint.pprint(boards, width=10)
