86cbc5dd6bd438235e94689f1bed87972bf96a02
Rich Wandell <richwandell@gmail.com>
2021-09-05 22:08:00 -0400

    #40

--- medium/40_combination_sum_II.py
+++ medium/40_combination_sum_II.py
@@ -6,38 +6,29 @@ class Solution:
 
         answers = []
         used_answers = {}
-        visited = {}
+        candidates.sort()
 
-        def backtrack(items=[], used_items=set(), total=0):
+        def backtrack(items=[], total=0, next_start=0):
 
-            def check_total():
-                if total == target:
-                    s = tuple(sorted(items[:]))
-                    if s not in used_answers:
-                        used_answers[s] = True
-                        answers.append(items[:])
-            check_total()
-            visited[tuple(sorted(used_items))] = total
+            if total == target:
+                s = tuple(sorted(items[:]))
+                if s not in used_answers:
+                    used_answers[s] = True
+                    answers.append(items[:])
 
             if total < target:
-                for i, num in enumerate(candidates):
-                    if i not in used_items:
-                        used_items.add(i)
-                        items.append(num)
-                        key = tuple(sorted(used_items))
-                        if key in visited:
-                            total = visited[key]
-                            check_total()
-                            if total != target:
-                                total -= num
-                        else:
-                            backtrack(items, used_items, total+num)
-                        items.pop()
-                        used_items.remove(i)
+                for i in range(next_start, len(candidates)):
+                    if i > next_start and candidates[i] == candidates[i-1]:
+                        continue
+                    items.append(candidates[i])
+                    backtrack(items, total+candidates[i], i+1)
+                    items.pop()
 
-        backtrack([], set(), 0)
+        backtrack([], 0, 0)
         return answers
 
 s = Solution()
-# print(s.combinationSum2([10,1,2,7,6,1,5], 8))
+print(s.combinationSum2([10,1,2,7,6,1,5], 8))
 print(s.combinationSum2([3,1,3,5,1,1], 8))
+print(s.combinationSum2([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], 27))
+print(s.combinationSum2([1,2], 4))
